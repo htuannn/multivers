204fb60dc7194c26d0df6594000009e31bfa0d99
Nguyen Hoang Tuan <102338877+htuannn@users.noreply.github.com>
2023-09-30 17:27:15 +0700

    +add get_longformer_phobert_checkpoint

--- multivers/util.py
+++ multivers/util.py
@@ -8,6 +8,36 @@ import pathlib
 import os
 import torch
 
+from transformers import RobertaForMaskedLM
+from transformers.models.longformer.modeling_longformer import LongformerSelfAttention
+
+class RobertaLongSelfAttention(LongformerSelfAttention):
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        past_key_value = None,
+        output_attentions=False,
+    ):
+        attention_mask = attention_mask.squeeze(dim=2).squeeze(dim=1)
+        is_index_masked = attention_mask < 0
+        is_index_global_attn = attention_mask > 0
+        is_global_attn = any(is_index_global_attn.flatten())
+        return super().forward(hidden_states, 
+                               is_index_masked=is_index_masked, 
+                               is_index_global_attn=is_index_global_attn, 
+                               is_global_attn=is_global_attn,
+                               attention_mask=attention_mask, 
+                               output_attentions=output_attentions)
+
+class RobertaLongForMaskedLM(RobertaForMaskedLM):
+    def __init__(self, config):
+        super().__init__(config)
+        for i, layer in enumerate(self.roberta.encoder.layer):
+            layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
 
 def load_jsonl(fname, max_lines=None):
     res = []
@@ -38,6 +68,13 @@ def write_jsonl(data, fname):
             print(json.dumps(line, cls=NPEncoder), file=f)
 
 
+def get_longformer_phobert_checkpoint():
+    """https://huggingface.co/bluenguyen/longformer-phobert-base-4096?fbclid=IwAR3ikj_MPxxJPY-Pb261_1J-sG5zTshZkUoNdkxsE2fXClza0M6nn_K7JYI&text=t%C3%B4i+l%C3%A0+%3Cmask%3E"
+    
+    loaded_model = RobertaLongForMaskedLM.from_pretrained("bluenguyen/longformer-phobert-base-4096")
+    checkpoint_model = loaded_model.state_dict()
+    return checkpoint_model
+    
 def get_longformer_science_checkpoint():
     current_dir = pathlib.Path(os.path.realpath(__file__)).parent
     fname = current_dir.parent / "checkpoints/longformer_large_science.ckpt"

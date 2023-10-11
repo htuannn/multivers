4914f0011b57bf30fec0e96c335ac14f04e72d7a
Nguyen Hoang Tuan <102338877+htuannn@users.noreply.github.com>
2023-10-11 13:08:31 +0700

    Update convertdata.py

--- convertdata.py
+++ convertdata.py
@@ -1,7 +1,9 @@
 import json
 import random
 #load jsonl file
+from sklearn.model_selection import train_test_split
 
+import pandas as pd
 
 def find_index_evidence(sentences, evidence):
     sentences = sentences.replace('...', '$$').strip()
@@ -23,9 +25,10 @@ def find_index_evidence(sentences, evidence):
 def convertfile(data, output):
     label_lookup = {"REFUTED": "REFUTES", "SUPPORTED": "SUPPORTS", "NEI": "NOT ENOUGH INFO"}
     with open(output, 'w', encoding = 'utf-8') as f:
-        for i in data:
-            k,v = list(i.items())[0]
-            id = k
+        data = pd.DataFrame.from_dict(data)
+        for _,v in data.iterrows():
+            id = v['id']
+
             claim = v['claim']
             label = label_lookup[v['verdict']]
 
@@ -48,6 +51,7 @@ def convertfile(data, output):
                 'negative_sample_id': negative_sample_id,
                 'abstract_id': abstract_id
             }
+
             json.dump(data_dict, f, ensure_ascii=False)
             f.write('\n')
         
@@ -55,19 +59,40 @@ def load_json(path_file):
     return json.load(open(path_file, "r", encoding="utf-8"))
 
 if __name__ == '__main__':
-    data = load_json('ise-dsc01-warmup.json')
-    key_id = data.keys()
-    
+    data_dict = load_json('ise-dsc01-train.json')
     train_size = 0.9
     dev_size = 0.1
     
-    data_list = [{v : k} for v, k in data.items()]
-   
-    random.shuffle(data_list)
-    train_data = data_list[:int(train_size*len(data_list))]
-    dev_data = data_list[int(train_size*len(data_list)):]
-    
-    
+    samples = [{
+        'id': i,
+        'claim': data_dict[i]['claim'],
+        'verdict': data_dict[i]['verdict'],
+        'context': data_dict[i]['context'],
+        'evidence': data_dict[i]['evidence']
+    } for i in data_dict.keys()]
+
+    # Tạo tập huấn luyện và tập kiểm tra với tỷ lệ nhãn giữ nguyên
+    X = [sample for sample in samples]
+    y = [sample['verdict'] for sample in samples]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=18)
+
+    train_data = {
+        'id': [sample['id'] for sample in X_train],
+        'claim': [sample['claim'] for sample in X_train],
+        'verdict': [sample['verdict'] for sample in X_train],
+        'context': [sample['context'] for sample in X_train],
+        'evidence': [sample['evidence'] for sample in X_train],
+    }
+
+    test_data = {
+        'id': [sample['id'] for sample in X_test],
+        'claim': [sample['claim'] for sample in X_test],
+        'verdict': [sample['verdict'] for sample in X_test],
+        'context': [sample['context'] for sample in X_test],
+        'evidence': [sample['evidence'] for sample in X_test],
+    }
+
     convertfile(train_data, 'train.jsonl')
-    convertfile(dev_data, 'dev.jsonl')
+    convertfile(test_data, 'dev.jsonl')
     
